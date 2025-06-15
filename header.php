22dba363daab02520c0d6687f147d4708fa3fe80
LucasAdamyMartin <lucasadamy123@gmail.com>
2025-06-15 14:36:17 +0200

    begin header

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <div class="logo">
+            <img src='images/Logo-IPMS-BLEU-scaled.webp' alt='logo' >
+        </div>
+        <nav>
+            <div>
+                <a>Formations</a>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                </svg>
+            </div>
+            <div>
+                <a>Ecoles</a>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                </svg>
+            </div>
+            <a>Actualités</a>
+            <a>Alternances/Emplois</a>
+            <a>Nos tarifs</a>
+            <a>Contact</a>
+            <a>FAQ</a>
+        </nav>
+        <div class="button-header">
+            <a>Espace candidat</a>
+        </div>
+    </div>
+</body>
+</html>
